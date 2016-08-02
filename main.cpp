d449d1fc5c3e96134277f57b5a5a1433d5d359eb
Martin Patzel <martin@patzel.de>
2016-08-02 14:22:02 +0200

    some cleanup

--- main.cpp
+++ main.cpp
@@ -88,35 +88,34 @@ void cnl0Change(void) {
 
 	// set OSCCAL frequency
 	if (uint8_t oscCal = hm.ee.getRegAddr(0,0,0,REG_CHN0_OSCCAL)) {
+	#ifdef SER_DBG
 		dbg << F("will set OSCCAL: old=") << OSCCAL << F(", new=") << oscCal << F("\n");
+	#endif
 		// Attention: your controller my have other factory calibration !!
-		// If you are unsure about the internal RC-frequency of your chip then comment out setting OSCCAL -
-		// use factory default instead!
+		// If you are unsure about the internal RC-frequency of your chip then use factory default (oscCal-Reg = 0)
 		// my chip: 1kHz - 8A=994Hz, 8B=998,4Hz, 8C=1001,6Hz, 8E=1010Hz
 		// frequency measured with help of millis-ISR (toggling LED port and measuring frequency on it)
 
 		OSCCAL = oscCal;
 	} else {
+	#ifdef SER_DBG
 		dbg << F("will set default OSCCAL: ") << getDefaultOSCCAL() << F("\n");
+	#endif
 		OSCCAL = getDefaultOSCCAL();
 	}
 	calibrateWatchdog();
 
 	// if burstRx is set ...
 	if (hm.ee.getRegAddr(0,0,0,REG_CHN0_BURST_RX)) {
+	#ifdef SER_DBG
 		dbg << F("PM=onradio\n");
+	#endif
 		hm.pw.setMode(POWER_MODE_WAKEUP_ONRADIO);											// set mode to wakeup on burst
 	} else {	// no burstRx wanted
-		// if no peer registered ==> then 8sec sleep is possible
-//		if (hm.ee.countFreeSlots(1) == hm.ee.getPeerSlots(1)) {
+		#ifdef SER_DBG
 			dbg << F("PM=8000ms\n");
+		#endif
 			hm.pw.setMode(POWER_MODE_WAKEUP_8000MS);										// set mode to awake every 8 secs
-//		} else {
-//			dbg << F("peers! setting burstRx=1\n");
-//			uint8_t arr[] = {REG_CHN0_BURST_RX, 0x01};
-//			hm.ee.setListArray(0,0,0,sizeof(arr),arr);										// set burstRx - is required when some peers are registered
-//			hm.pw.setMode(POWER_MODE_WAKEUP_ONRADIO);										// set power management mode
-//		}
 	}
 
 	// fetch transmitDevTryMax
