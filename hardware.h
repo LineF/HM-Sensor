89d2c0e748b9134ea13eba39f0953410d2c4501a
Martin Patzel <martin@patzel.de>
2017-01-23 23:54:13 +0100

    some cleanup

--- hardware.h
+++ hardware.h
@@ -12,30 +12,4 @@
 	#define DEBOUNCE                   5
 	#define TIMER2_LOW_FREQ_OSC
 
-	#if defined(__AVR_ATmega328P__)
-		//- hardware specific general setup
-		#define PCINT_CALLBACK													// enables the pin change interrupt callback in user sketch
-
-		//- configuration key  ----------------------------------------------------------------------------------------
-		//#define CONFIG_KEY             PIN_B0									// define the config key pin port
-
-	#elif defined(__AVR_ATmega32U4__)
-		//- configuration key  ----------------------------------------------------------------------------------------
-		//#define CONFIG_KEY             PIN_B6									// define the config key pin port
-
-	#else
-		#error "Error: cc1100 CS and GDO0 not defined for your hardware in hardware.h!"
-	#endif
-	//- ---------------------------------------------------------------------------------------------------------------
-
-
-	//- wake up pin ---------------------------------------------------------------------------------------------------
-	#if defined(__AVR_ATmega32U4__)
-		#define WAKE_UP_DDR            DDRE										// define wake up pin port and remaining pin
-		#define WAKE_UP_PORT           PORTE
-		#define WAKE_UP_PNR            PINE
-		#define WAKE_UP_PIN            PINE2
-	#endif
-	//- ---------------------------------------------------------------------------------------------------------------
-
 #endif
